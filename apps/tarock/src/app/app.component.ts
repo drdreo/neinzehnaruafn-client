e2e66a8457264cceb28a085183d9a3f0e49e5cf8
DrDreo <code@andreas-hahn.at>
2021-05-15 12:23:00 +0200

    initial commit

---
+++ apps/tarock/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trial-nerror-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tarock';
+}
